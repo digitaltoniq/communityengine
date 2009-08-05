665f1fba51138250ffd786d1014aa02b5a86f6ab
Ryan Daigle <ryan.daigle@gmail.com>
2009-08-05 09:48:30 -0400

    Fix metro area factory

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090801220755) do
+ActiveRecord::Schema.define(:version => 20090801214622) do
 
   create_table "activities", :force => true do |t|
     t.integer  "user_id"
