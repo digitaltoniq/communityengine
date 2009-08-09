435f59388c6cd234e3570adeb25761a2f69b5e3d
Daniel Snider <dsnider@digitaltoniq.com>
2009-08-09 18:22:54 -0400

    Removing old slugable hack

--- plugins/acts_as_slugable_hacks/lib/acts_as_slugable_hacks.rb
+++
@@ -1,8 +0,0 @@
-Multiup::Acts::Slugable::InstanceMethods.module_eval do
-
-  # acts_as_slug needs source tp be an attribute, this hack allows methods too
-  def read_attribute(attr_name)
-    attr_name.to_sym == source_column.to_sym ? self.send(source_column.to_sym) : super
-  end
-  
-end
