42a229151ad3b4af3a8200e96b3eabcaf2249c47
Ryan Daigle <ryan.daigle@gmail.com>
2009-10-15 13:28:21 -0400

    Remove non-default 'since' option.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -177,7 +177,7 @@ class PostsController < BaseController
 
 
   def popular
-    @posts = Post.find_popular({:limit => 10, :since => 3.days})
+    @posts = Post.find_popular({:limit => 10})
 
 #    @monthly_popular_posts = Post.find_popular({:limit => 20, :since => 30.days})
     
