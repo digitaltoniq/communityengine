b2662ea99ea8dfb5ad03647cf81c9b181d8c2439
Ryan Daigle <ryan.daigle@gmail.com>
2009-10-15 13:28:45 -0400

    Update environment absolute URLs. [#174 state:resolved]

--- config/environments/client.rb
+++ config/environments/client.rb
@@ -27,4 +27,4 @@ config.action_view.cache_template_loading            = true
 # Enable threaded mode
 # config.threadsafe!
 
-APP_URL = "http://candme.digitaltoniq.com"
+APP_URL = "http://candme.clients.digitaltoniq.com"
