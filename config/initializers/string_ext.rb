be36183af17d9e0777001803cbf6c0506e97e797
Ryan Daigle <ryan.daigle@gmail.com>
2009-11-24 11:15:39 -0500

    Fix following company post notification email. [#243]

--- config/initializers/string_ext.rb
+++ config/initializers/string_ext.rb
@@ -2,4 +2,10 @@ class String
   def strip_html
     @strip_html ||= self.gsub(%r{</?[^>]+?>}, '').strip
   end
+
+  def truncate_words(length = 30, end_string = '...')
+    return if self.blank?
+    words = self.split()
+    words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
+  end
 end
