b51e6f4563d71cb48dae78fb18b8744112420610
Ryan Daigle <ryan.daigle@gmail.com>
2010-01-06 19:15:53 -0500

    Update mail to use CAM authsmtp account. [#284]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,4 +27,4 @@ config.action_view.cache_template_loading            = true
 # Enable threaded mode
 # config.threadsafe!
 
-APP_URL = "http://companiesandme.com"
+APP_URL = "http://preview.companiesandme.com"
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -1,11 +1,11 @@
 unless Rails.env.test? or Rails.env.cucumber?
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-          :domain => 'digitaltoniq.com',
+          :domain => 'companiesandme.com',
           :address => 'mail.authsmtp.com',
           :port => 2525,
           :authentication => :login,
-          :user_name => 'ac43514 ',
-          :password => 'pshp7wfhh'
+          :user_name => 'ac46789',
+          :password => 'camlive2010!!'
   }
 end
