e0d6ef65e443546ec3a159566208c22bad947f8f
Vlad Alive <vladalive@gmail.com>
2010-01-15 14:06:48 +0300

    Fix: design form errors;

--- config/environment.rb
+++ config/environment.rb
@@ -83,3 +83,15 @@ Sass::Plugin.options[:template_location] = {
    "#{RAILS_ROOT}/themes/#{AppConfig.theme}/stylesheets/sass" => "#{RAILS_ROOT}/themes/#{AppConfig.theme}/stylesheets"
 }
 
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  error_class = "fieldWithErrors"
+  if html_tag =~ /<(input|textarea|select)[^>]+class=/
+    class_attribute = html_tag =~ /class=['"]/
+    html_tag.insert(class_attribute + 7, "#{error_class} ")
+  elsif html_tag =~ /<(input|textarea|select)/
+    first_whitespace = html_tag =~ /\s/
+    html_tag[first_whitespace] = " class='#{error_class}' "
+  end
+  html_tag
+end
+
